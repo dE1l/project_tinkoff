982a61d665e6d9a45f8a496fc580e7bee3161f55
Roman <dell.oxl@gmail.com>
2017-11-29 20:01:57 +0300

    + clearing

--- conftest.py
+++ conftest.py
@@ -40,16 +40,3 @@ def api(optns, request):
     """
     allure.environment(report='Allure report', server=optns['base_url'])
     return Application(optns)
-
-
-# @pytest.fixture(scope="function")
-# def log(request):
-#     log_capture_string = io.StringIO()
-#     return logging.config.dictConfig(log_test(log_capture_string))
-
-# def attach_screenshot(screenshot_file_name='screenshot %s' % datetime.now().strftime("%Y-%m-%d %H:%M:%S")):
-#     return allure.attach(screenshot_file_name, driver.get_screenshot_as_png(), type=AttachmentType.PNG)
-
-
-# def attach_console_log():
-#     return allure.attach('console.log', str(driver.get_log('browser')))
